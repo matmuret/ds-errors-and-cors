8b57cbebb7104294116c761b1ed74162a2b4321d
Filippo <filippo.moretti6@gmail.com>
2020-10-15 15:41:06 +0200

    ...

--- server.js
+++ server.js
@@ -17,19 +17,23 @@ app.listen(5000, () => {
 // success: next()
 // error: res.json({error: "You did wrong!"})
 
+//1 
 // 404 handler => kicks in if we did not found any matching route handler
-app.use( (req, res, next) => {
+/* app.use( (req, res, next) => {
   res.status(404).json({ error: `Route ${req.url} does not exist`})
-})
+}) */
 
-app.get("/fuckup/saw-it-coming", (req, res, next) => {
+//2
+/* app.get("/fuckup/saw-it-coming", (req, res, next) => {
   try {
-    booh;
+    res.json({name:"example"})
   } catch (err) {
     next(err);
   }
 });
+ */
 
+//3
 app.get("/fuckup/surprise", (req, res, next) => {
   throw new Error("<your error message>");
 });
